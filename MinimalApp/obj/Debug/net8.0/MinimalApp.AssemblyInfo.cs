826674d8e537a4161d286a75267d31adfea06f19
Alexandre Baccarini <AlexandreBaccaJr@gmail.com>
2025-04-01 22:14:40 -0300

    adicionando gitignore

--- MinimalApp/obj/Debug/net8.0/MinimalApp.AssemblyInfo.cs
+++ MinimalApp/obj/Debug/net8.0/MinimalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d0d38e32e2c10f967ae3efa53da5f116286301")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
